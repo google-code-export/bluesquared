938cf88493a0449a2675709361b0cd8ab8358b60
casey.ackels <casey.ackels@ce48681f-eaa5-8884-e2bf-b58722ad4a34>
2011-07-08 20:37:46 +0000

    Initial commit for the About Window package

---
+++  bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/Libraries/about/about_gui.tcl
@@ -0,0 +1,64 @@
+# Creator: Casey Ackels
+# Initial Date: April 8th, 2011
+# Dependencies: about_code.tcl, package.tcl
+# Notes: This is a complete package to build your own About Window
+#-------------------------------------------------------------------------------
+#
+# Subversion
+#
+# $Revision: 50 $
+# $LastChangedBy: casey.ackels $
+# $LastChangedDate: 2011-03-13 17:09:18 -0700 (Sun, 13 Mar 2011) $
+#
+########################################################################################
+
+##
+## - Overview
+# This file holds the parent GUI frame, buttons and menu for Distribution Helper
+
+# Definitions for prefixes of Variables
+# G = Global
+# S = String
+# L = List
+# I = Integer (Do not use this unless you are certain it is an Integer and not a plain string)
+
+## Coding Conventions
+# - Namespaces: 
+
+# - Procedures: Proc names should have two words. The first word lowercase the first character of the first word,
+#   will be uppercase. I.E sourceFiles, sourceFileExample
+
+namespace eval BlueSquared_About {}
+    
+    # Source Dependencies
+    source about_code.tcl
+
+
+proc BlueSquared_About::aboutWindow {} {
+    #****f* aboutWindow/BlueSquared_About
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2011 - Casey Ackels
+    #
+    # FUNCTION
+    #	
+    #
+    # SYNOPSIS
+    #	GUI for the About window
+    #
+    # CHILDREN
+    #	
+    #
+    # PARENTS
+    #	
+    #
+    # NOTES
+    #
+    # SEE ALSO
+    #
+    #***
+    
+    
+} ;# End aboutWindow
