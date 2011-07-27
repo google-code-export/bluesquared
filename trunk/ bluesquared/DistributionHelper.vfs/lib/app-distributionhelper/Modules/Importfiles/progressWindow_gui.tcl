02dac7146fae3eb5233f8c6d63c64a8b6a437eb6
casey.ackels <casey.ackels@ce48681f-eaa5-8884-e2bf-b58722ad4a34>
2011-07-27 21:06:48 +0000

    Completely rewrote how the ShipVia switch works. Placed all of that code in its own proc.
    Implemented a list of possible 3rd Party matches
    
    Implemented a list of possible 3rd party ship via codes. These will need to be displayed for the user to edit the list

--- trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/Modules/Importfiles/progressWindow_gui.tcl
+++ trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/Modules/Importfiles/progressWindow_gui.tcl
@@ -22,13 +22,13 @@
 # I = Integer (Do not use this unless you are certain it is an Integer and not a plain string)
 
 ## Coding Conventions
-# - Namespaces: 
+# - Namespaces:
 
 # - Procedures: Proc names should have two words. The first word lowercase the first character of the first word,
 #   will be uppercase. I.E sourceFiles, sourceFileExample
 
 
-proc Disthelper_GUI::progressWindow {} { 
+proc Disthelper_GUI::progressWindow {} {
     #****f* progressWindow/Disthelper_GUI
     # AUTHOR
     #	Casey Ackels
@@ -46,7 +46,7 @@ proc Disthelper_GUI::progressWindow {} {
     #	N/A
     #
     # PARENTS
-    #	
+    #
     #
     # NOTES
     #
@@ -54,68 +54,68 @@ proc Disthelper_GUI::progressWindow {} {
     #
     #***
     global GL_file program
+    destroy .progress
+
     toplevel .progress
     wm title .progress [mc "Processing File"]
     wm transient .progress .
-    
+
     focus .progress
-    
+
     set locX [expr {([winfo screenwidth .] - [winfo width .]) / 2}]
     set locY [expr {([winfo screenheight .] - [winfo height .]) / 2}]
     wm geometry .progress +${locX}+${locY}
-    
+
     # Reset variables
     set program(ProgressBar) 0
     set program(totalAddress) 0
     set program(totalBoxes) 0
     set program(totalBooks) 0
     set program(totalAddress) 0
-    set program(maxAddress) [expr {[llength $GL_file(dataList)] - 1}]
-    
+    #set program(maxAddress) [expr {[llength $GL_file(dataList)] - 1}]
+    set program(maxAddress) [llength $GL_file(dataList_modified)]
+
     ##
     ## Parent Frame
     ##
     set container [ttk::labelframe .progress.container -text [mc "File Progress"]]
     pack $container -expand yes -fill both -pady 5p -padx 5p
-    
+
     set frame1 [ttk::frame $container.frame1]
     pack $frame1 -expand yes -fill both -pady 5p -padx 5p
-    
+
     #ttk::progressbar $frame1.bar -length [llength $GL_file(dataList)]
     ttk::progressbar $frame1.bar -mode determinate -variable program(ProgressBar) -maximum $program(maxAddress)
     pack $frame1.bar -fill x -expand 1 -padx 4p -pady 10p
-    
-    
+
+
     set frame2 [ttk::frame $container.frame2]
     pack $frame2 -expand yes -fill both -pady 5p -padx 5p
-    
+
     ttk::label $frame2.txt1a -text [mc "Addresses"]
     ttk::label $frame2.txt1b -textvariable program(totalAddress)
-    
+
     ttk::label $frame2.txt2a -text [mc "Boxes"]
     ttk::label $frame2.txt2b -textvariable program(totalBoxes)
-    
+
     ttk::label $frame2.txt3a -text [mc "Books"]
     ttk::label $frame2.txt3b -textvariable program(totalBooks)
-    
+
     grid $frame2.txt1a -column 0 -row 0 -sticky nse -padx 4p
     grid $frame2.txt1b -column 1 -row 0 -sticky nsw
-    
+
     grid $frame2.txt2a -column 0 -row 1 -sticky nse -padx 4p
     grid $frame2.txt2b -column 1 -row 1 -sticky nsw
-    
+
     grid $frame2.txt3a -column 0 -row 2 -sticky nse -padx 4p
     grid $frame2.txt3b -column 1 -row 2 -sticky nsw
-    
+
     set btnbar [ttk::frame .progress.btnbar]
     pack $btnbar -padx 4p -pady 10p -side right
-    
+
     ttk::button $btnbar.close -text [mc "Close"] -command {destroy .progress}
 
     grid $btnbar.close -column 0 -row 0 -sticky nse
-    
+
     Disthelper_Code::writeOutPut
 }
-
-
-
--- trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/startup.tcl
+++ trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/startup.tcl
@@ -59,7 +59,7 @@ proc 'distHelper_sourceReqdFiles {} {
     #
     #***
 	## All files that need to be sourced should go here. That way if any of them fail to load, we'll catch it.
-	
+
 	#Modify the Auto_path so our 'package requires' work.
         ##
         ## Binaries
@@ -68,7 +68,7 @@ proc 'distHelper_sourceReqdFiles {} {
         lappend ::auto_path [file join [file dirname [info script]] Binaries]
         #lappend ::auto_path [file join [file dirname [info script]] Binaries sqlite3.3.8]
 	lappend ::auto_path [file join [file dirname [info script]] Binaries tkdnd2.2]
-        
+
 	##
         ## 3rd party tcl scripts
         ##
@@ -79,21 +79,21 @@ proc 'distHelper_sourceReqdFiles {} {
 	lappend ::auto_path [file join [file dirname [info script]] Libraries tooltip]
         lappend ::auto_path [file join [file dirname [info script]] Libraries tcom3.9]
         lappend ::auto_path [file join [file dirname [info script]] Libraries about]
-	
+
 	##
         ## Project built scripts
         ##
 	lappend ::auto_path [file join [file dirname [info script]] Modules]
 	lappend ::auto_path [file join [file dirname [info script]] Modules Core]
 	lappend ::auto_path [file join [file dirname [info script]] Modules Importfiles]
-	
+
 	#
 	## Start the Package Require
 	#
-	
+
 	## System Packages
 	package require msgcat
-	
+
 	## 3rd Party modules
 	package require tkdnd
 	#package require Tablelist_tile 5.2
@@ -101,18 +101,18 @@ proc 'distHelper_sourceReqdFiles {} {
 	package require autoscroll
 	package require csv
         package require tcom
-	
+
 	## Distribution Helper modules
 	package require disthelper_core
 	package require disthelper_importFiles
         package require aboutwindow
 
-	
+
 	# Source files that are not in a package
         source [file join [file dirname [info script]] Libraries popups.tcl]
         source [file join [file dirname [info script]] Libraries errorMsg_gui.tcl]
         source [file join [file dirname [info script]] Libraries debug.tcl]
-        
+
         #namespace import dh_Debug::debug
         'debug "Loaded"
 }
@@ -149,7 +149,7 @@ proc 'distHelper_sourceOtherFiles {} {
 }
 
 
-proc 'distHelper_initVariables {} { 
+proc 'distHelper_initVariables {} {
     #****f* initVariables/Disthelper_Helper
     # AUTHOR
     #	Casey Ackels
@@ -158,7 +158,7 @@ proc 'distHelper_initVariables {} {
     #	(c) 2011 - Casey Ackels
     #
     # FUNCTION
-    #	
+    #
     #
     # SYNOPSIS
     #	Initialize program defaults. Create new file if one does not exist.
@@ -167,7 +167,7 @@ proc 'distHelper_initVariables {} {
     #	N/A
     #
     # PARENTS
-    #	
+    #
     #
     # NOTES
     #
@@ -175,47 +175,47 @@ proc 'distHelper_initVariables {} {
     #
     #***
     global settings header
-    
+
     # hackish, but this will allow us to add new defaults/settings without killing an existing config file.
     if {![info exists settings(Home)]} {
         # Application location
         set settings(Home) [pwd]
     }
-    
+
     if {![info exists settings(outFilePath)]} {
         # Location for saving the file
         set settings(outFilePath) [file dirname $settings(Home)]
     }
-    
+
     if {![info exists settings(outFilePathCopy)]} {
         # Location for saving a copy of the file (this should just be up one directory)
         set settings(outFilePathCopy) [file dirname $settings(Home)]
     }
-    
+
     if {![info exists settings(sourceFiles)]} {
         # Default for finding the source import files
         set settings(sourceFiles) [file dirname $settings(Home)]
     }
-    
+
     if {![info exists settings(importOrder)]} {
         # Set default for headers; wording is used internally
         set settings(importOrder) [list shipVia Company Consignee delAddr delAddr2 delAddr3 City State Zip Phone Quantity Version Date Contact Email 3rdParty]
     }
-    
+
     if {![info exists settings(shipvia3P)]} {
         # Set possible 3rd party shipvia codes
-        set settings(shipvia3P) [list 018 068 154 166]
+        set settings(shipvia3P) [list 068 067 154 166]
     }
-    
+
     if {![info exists settings(BoxTareWeight)]} {
         # Box Tare Weight
         set settings(BoxTareWeight) .566
     }
-    
+
     if {![info exists header(shipvia)]} {
         set header(shipvia) [list "ship via" shipvia]
     }
-    
+
     if {![info exists header(company)]} {
         set header(company) [list company destination "company name"]
     }
@@ -224,37 +224,41 @@ proc 'distHelper_initVariables {} {
         # Variations on spelling of consignee
         set header(attention) [list contact attention attn attn:]
     }
-    
+
     if {![info exists header(address1)]} {
         set header(address1) [list address address1 "address 1" add add1 "add 1" addr addr1 "addr 1"]
     }
-    
+
     if {![info exists header(address2)]} {
         set header(address2) [list address2 "address 2" add2 "add 2" addr2 "addr 2"]
     }
-    
+
     if {![info exists header(address3)]} {
         set header(address3) [list address3 "address 3" add3 "add 3" addr3 "addr 3"]
     }
-    
+
     if {![info exists header(CityStateZip)]} {
         set header(CityStateZip) [list city-state-zip city-st-zip "city state zip" "city st zip" csv state/region]
     }
-    
+
     if {![info exists header(state)]} {
         set header(state) [list st st. state]
     }
-    
+
     if {![info exists header(quantity)]} {
         set header(quantity) [list quantity qty]
     }
-    
+
     if {![info exists header(version)]} {
         set header(version) [list version vers]
     }
-    
+
     if {![info exists header(zip)]} {
         set header(zip) [list zip zipcode "zip code" postalcode "postal code" postal]
+    }
+
+    if {![info exists header(3rdPartyNumber)]} {
+        set header(3rdPartyNumber) [list "3rd Party" 3rdParty 3p]
     }
 }
 
@@ -287,41 +291,41 @@ proc 'distHelper_loadSettings {} {
     #
     #***
     global settings debug program header
-    
+
     # Enable / Disable Debugging
     # See 'distHelper_sourceReqdFiles for the [namespace import] command
     set debug(onOff) on
-    
+
     set program(Name) "Efficiency Assist"
-    
+
     # Theme setting for Tile
     #ttk::style theme use xpnative
-    
+
     # Import msgcat namespace so we only have to use [mc]
     namespace import msgcat::mc
-    
+
     if {[catch {open config.txt r} fd]} {
 	puts "unable to load defaults"
         puts "execute initVariables"
-        
-        # Initialize default values 
+
+        # Initialize default values
         'distHelper_initVariables
-        
+
         #Disthelper_Preferences::saveConfig
-        
+
     } else {
 	set configFile [split [read $fd] \n]
 	catch {chan close $fd}
-	
+
 	foreach line $configFile {
 	    if {$line == ""} {continue}
 	    set l_line [split $line " "]
 	    set [lindex $l_line 0] [join [lrange $l_line 1 end] " "]
 	}
-        
+
         # Check to see if we have new default settings
         'distHelper_initVariables
-        
+
         puts "Loaded variables"
     }
 }
