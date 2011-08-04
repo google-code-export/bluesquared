b48d64ac043990378c51a1730da9cf1241149918
casey.ackels <casey.ackels@ce48681f-eaa5-8884-e2bf-b58722ad4a34>
2011-08-04 22:25:58 +0000

    First round of implementing smart formatting to the ChangeLog

--- trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/Libraries/about/about_code.tcl
+++ trunk/ bluesquared/DistributionHelper.vfs/lib/app-distributionhelper/Libraries/about/about_code.tcl
@@ -74,9 +74,44 @@ proc BlueSquared_About::aboutOpenFiles {about_Text changeLog_Text} {
     set fileChangeLog [file join $starkit::topdir lib app-distributionhelper CHANGELOG.txt]
     set openChangeLog [open $fileChangeLog RDONLY]
 
+    # Make the markup tags
+    $changeLog_Text tag config Heading1 -font {verdana 12 bold}
+    $changeLog_Text tag config Heading2 -font {verdana 10 bold}
+    $changeLog_Text tag config Normal -font {arial 10}
+    $changeLog_Text tag config List1 -font {verdana 9} -lmargin1 8 -lmargin2 12
+    $changeLog_Text tag config List2 -font {verdana 9} -lmargin1 8 -lmargin2 12
 
+    ttk::separator $changeLog_Text.sep -orient horizontal
+    #pack $changeLog_Text.sep
+
+    set i 0
+    set idx 0
     while { [gets $openChangeLog line] >= 0 } {
-        $changeLog_Text insert end $line\n
+        # Keep track of how many lines we insert
+        incr idx
+        #'debug StringRange: [string range $line 0 1]
+        # Capture our 'markup' and format the text
+        # = Heading
+        # + 2nd Heading
+        # * Non-numbered list
+        # # Numbered list
+
+        set stringFormat [string range $line 2 end]
+        # \u25C6 ;# Diamond
+        # \u00B7 ;# Small
+        # \U2022 ;# unicode
+
+        switch -- [string range $line 0 1] {
+            "= "    {set textFormat Heading1; set i 0 ;# Heading1}
+            "+ "    {set textFormat Heading2; set i 0 ;# Heading2}
+            "* "    {set stringFormat "\u2022 $stringFormat"; set textFormat List1; set i 0 ;# List1}
+            "# "    {set stringFormat "[incr i]: $stringFormat"; set textFormat List2;# List2}
+            "--"    {set stringFormat [$changeLog_Text window create $idx.end -window $changeLog_Text.sep -stretch yes]}
+            default {set stringFormat $line; set textFormat Normal; set i 0;# No markup detected}
+        }
+
+        $changeLog_Text insert end $stringFormat $textFormat
+        $changeLog_Text insert end \n
     }
 
     # Since we inserted our text, lets disable the widget to prevent the deletion of text (not that it matters, but adds a bit of professionalism)
