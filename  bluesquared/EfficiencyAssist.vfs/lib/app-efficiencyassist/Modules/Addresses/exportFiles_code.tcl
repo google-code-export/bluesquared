e0e64967f16af2c750b4681c016a3c2b39b479c2
casey.ackels@gmail.com <casey.ackels@gmail.com@ce48681f-eaa5-8884-e2bf-b58722ad4a34>
2014-01-12 19:21:47 +0000

    exportFiles_code.tcl - added

---
+++  bluesquared/EfficiencyAssist.vfs/lib/app-efficiencyassist/Modules/Addresses/exportFiles_code.tcl
@@ -0,0 +1,46 @@
+# Creator: Casey Ackels
+# Initial Date: March 12, 2011]
+# File Initial Date: 01 12,2014
+# Dependencies: 
+#-------------------------------------------------------------------------------
+#
+# Subversion
+#
+# $Revision: 169 $
+# $LastChangedBy: casey.ackels $
+# $LastChangedDate: 2011-10-17 16:11:20 -0700 (Mon, 17 Oct 2011) $
+#
+########################################################################################
+
+proc importFiles::exportFiles {} {
+    #****f* exportFiles/importFiles
+    # AUTHOR
+    #	Casey Ackels
+    #
+    # COPYRIGHT
+    #	(c) 2011-2014 Casey Ackels
+    #
+    # FUNCTION
+    #	Export the data into a CSV file (eventually should allow the user to select the filetype that they want.)
+    #
+    # SYNOPSIS
+    #
+    #
+    # CHILDREN
+    #	N/A
+    #
+    # PARENTS
+    #	
+    #
+    # NOTES
+    #
+    # SEE ALSO
+    #
+    #***
+    global log
+    ${log}::debug --START-- [info level 1]
+    
+    
+	
+    ${log}::debug --END-- [info level 1]
+} ;# importFiles::exportFiles
